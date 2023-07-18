45f363d325572f61ef12d676ec3b773b4fe1f0ea
Abdullah-Nazardeen <anazardeenace@gmail.com>
2023-07-18 11:24:11 +0530

    seveth commit

--- components/MovieSummary.js
+++ components/MovieSummary.js
@@ -1,6 +1,6 @@
 import MovieCard from "./MovieCard";
 
-const MovieSummary = ({ movies }) => {
+const MovieSummary = ({ movies = [] }) => {
   return (
     <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
       <h1 className="text-white text-2xl mt-8 mb-5">Movie Summary's List:</h1>
